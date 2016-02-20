d2ede3e98d36b8b4efd0720b0c2ad35dab0df11a
tfarrell-vz <farrell@abiresearch.com>
2016-02-20 16:00:55 -0500

    Add eventEmitter function and a test event object.

--- nectour.js
+++ nectour.js
@@ -10,11 +10,28 @@ var Event = {
     // date
 };
 
+var eventEmitter = function(event, target) {
+    console.log(JSON.stringify(event));
+    
+    // TODO: Send request to Django app to add event to database.         
+};
+
 request(url, (error, response, body) => {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(body);
-    }    
+    }
+    
+    // TODO: Dissect response to unpack information. 
+    
 });
 
+var testEvent = function() {
+    var event = Object.create(Event);
+    event.venue = "Bass Center 2016";
+    event.city = "Denver";
+    event.state = "Colorado";
+
+    eventEmitter(event, url);    
+}();
 
 
